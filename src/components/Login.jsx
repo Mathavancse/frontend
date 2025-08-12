f4db203b05b2bc070911b9cccc3de3b9461743ca
mathavan <kmathavancse@gmail.com>
2025-08-13 04:04:48 +0530

    dsds

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
         }}
       >
         <Typography variant="h5" fontWeight="bold" mb={2} align="center">
-          Login
+          Loginvvv
         </Typography>
 
         <form onSubmit={handleSubmit}>
