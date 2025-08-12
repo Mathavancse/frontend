31cbfacb267be85355d2437ab6b23b9587aa11c2
mathavan <kmathavancse@gmail.com>
2025-08-12 19:53:32 +0530

    pasasasas

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -28,7 +28,7 @@ const login = () => {
         <label>emailemail</label>
         <input type="text" onChange={(e) => setemail(e.target.value)} />
         <br />
-        <label>Passworddfdf</label>
+        <label>Passworddfdhjhjhjjhjf</label>
         <input type="text" onChange={(e) => setpassword(e.target.value)} />
         <button type="submit">submit</button>
       </form>
